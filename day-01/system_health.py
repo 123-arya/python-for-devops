1900fd91a1a3a0018e9e6af3f16756b742e8a098
123-arya <158812619+123-arya@users.noreply.github.com>
2026-02-04 17:51:39 +0530

    Update system_health.py

--- day-01/system_health.py
+++ day-01/system_health.py
@@ -1,2 +1,22 @@
 # Introduction to Python for DevOps
 
+import psutil
+
+def check_cpu_threshold():
+    cpu_threshold = int(input("Enter the cpu threshold:"))
+
+    current_cpu = psutil.cpu_percent(interval=1)
+    print("current cpu usage:", current_cpu)
+    
+    if current_cpu > cpu_threshold:
+        print("Alert on email")
+    else:
+        print("safe")
+check_cpu_threshold()
+
+Output:
+
+PS C:\Users\Satish_More\Documents\Josh-pythondevops\Day-01\practice> python .\cpu_usage.py       
+Enter the cpu threshold:20                                                                       
+current cpu usage: 3.2
+safe
