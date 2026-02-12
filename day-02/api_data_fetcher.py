2f59b9c37a163695caf52b0d8614b62eab93a48c
123-arya <158812619+123-arya@users.noreply.github.com>
2026-02-12 17:27:24 +0530

    Update api_data_fetcher.py

--- day-02/api_data_fetcher.py
+++ day-02/api_data_fetcher.py
@@ -1,1 +1,26 @@
+import requests
 
+api_url= "https://fake-json-api.mock.beeceptor.com/users"
+
+response=requests.get(url=api_url)
+
+for url in response.json():
+    if url["id"] == 1:
+        print("found")
+    else:
+        print("not found")
+
+print(response.json())
+
+Output:
+PS C:\Users\Satish_More\Documents\Josh-pythondevops\python-for-devops\day-02> python .\API.PY
+not found
+not found
+not found
+not found
+not found
+not found
+found
+not found
+not found
+not found
